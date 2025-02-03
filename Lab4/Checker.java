9509d2f22f1b0a4d46da75f7d0f3caf411da84ef
sachin-bhusal <bhusalsachin189@gmail.com>
2025-02-03 18:25:05 -0500

    Lab 4 uploded

---
+++ Lab4/Checker.java
@@ -0,0 +1,46 @@
+package Lab4;
+
+public class Checker
+{
+   public static void main(String [] args)
+   {
+    MyConsole console = new MyConsole();
+    System.out.println("Welcome to Odd/Even Checker!");
+
+    boolean continueChecking = true;
+    while(continueChecking)
+    {
+        int number = console.getInt("Enter an integer: ");
+
+        if(number % 2 == 0)
+        {
+            System.out.println("The number" + number + "is even.");
+        }
+        else 
+        {
+            System.out.println("The number" + number + "is odd number.");
+        }
+    }
+    String choice;
+    while(true)
+    {
+        choice = console.getString("Continue? (y/n): ").toLowerCase();
+        if(choice.equals("y"))
+        {
+            break;
+        }
+        else if (choice.equals("n"))
+        {
+            continueChecking = false;
+            break;
+        }
+        else
+        {
+            System.out.println("Error! Enter 'y' or 'n'. Try again.");
+        }
+    }
+
+    console.close();
+    System.out.println("Thank you for using Odd/Even Checker!");
+   }
+}
---
+++ Lab4/MyConsole.java
@@ -0,0 +1,28 @@
+package Lab4;
+
+public class MyConsole extends Console {
+    public MyConsole() 
+    {
+        super();
+    }
+
+    @Override
+    public String getString(String prompt) 
+    {
+        String s = "";
+        boolean isValid = false;
+        while (isValid == false) 
+        {
+            s = super.getString(prompt);
+            if (s.equals("")) 
+            {
+                System.out.println("Error! This entry is required. Try again.");
+            } 
+            else 
+            {
+                isValid = true;
+            }
+        }
+        return s;
+    }
+}
