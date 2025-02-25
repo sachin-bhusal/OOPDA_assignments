1e4160dfb0f0be5e95e02b01c26d13a124164fbd
sachin-bhusal <bhusalsachin189@gmail.com>
2025-02-24 21:30:53 -0500

    changed AnimalCounterApp

--- Lab11/AnimalCounterApp.java
+++ Lab11/AnimalCounterApp.java
@@ -1,28 +1,29 @@
 package Lab11;
 
+
 public class AnimalCounterApp 
 {
-    public static void count(Countable c, int maxCount) 
-    {
-        c.resetCount();
-    
-        String animalType = (c instanceof Sheep) ? "sheep" : c.getCountString().split(" ")[1];
-    
-        System.out.println("\nCounting " + animalType + "...");
-        for (int i = 1; i <= maxCount; i++)
-        {
-            c.incrementCount();
-            System.out.println(c.getCountString());
+    public static void countingAll(Countable c, int maxCount)
+        { 
+            c.resetCount();
+            for(int i = 0; i < maxCount; i++)
+            { 
+                c.incrementCount(); 
+                System.out.println(c.getCountString());
+            }
+            
+            
         }
-    }
 
-    public static void main(String[] args) 
-    {
-        Countable alligator = new Alligator();
-        count(alligator, 3);
-
-        Sheep sheep = new Sheep("Blackie");
-        count(sheep, 2);
-    }
-}
-
+        public static void main(String[] args)
+        { 
+            Alligator alligator = new Alligator(); 
+            System.out.println("\nAlligator Counting.....");
+            AnimalCounterApp.countingAll(alligator, 2);
+            System.out.println();
+        
+            System.out.println("Counting Sheep.....");
+            Sheep sheep = new Sheep("Blackie"); 
+            AnimalCounterApp.countingAll(sheep, 3);
+        }
+}        
